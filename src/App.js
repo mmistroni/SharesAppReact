3aa5aca4bd0303b24f0871cd7c38c26e6fde519a
mmistroni <mmistroni@gmail.com>
2019-03-21 21:34:27 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem, NavbarToggler, Collapse } from 'reactstrap';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar dark color="dark">
+            <NavbarBrand href="/">Shares App</NavbarBrand>
+            <NavbarToggler/>
+            <Collapse isOpen="true" navbar>
+                <Nav left>
+                    <NavItem>
+                        <NavLink href="/components/">Shares</NavLink>
+                   </NavItem>
+                   <NavItem>
+                        <NavLink href="https://github.com/reactstrap/reactstrap">Portfolio</NavLink>
+                   </NavItem>
+                   <NavItem>
+                        <NavLink href="https://github.com/reactstrap/reactstrap">Trade Notes</NavLink>
+                   </NavItem>
+                </Nav>
+            </Collapse>
+             
+        </Navbar>
       </div>
     );
   }
 }
-
 export default App;
